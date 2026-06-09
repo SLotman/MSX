2a54d175a7d1a5f464f6376a60b2f05fe3a9590f
SLotman <slotmanrj@yahoo.com.br>
2026-06-09 02:19:13 -0300

    tests on new machine

--- blueMSX-DPI/Src/BuildInfo/build_number.h
+++ blueMSX-DPI/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 35027
+#define BUILD_NUMBER 35028
 
-#define BUILD_DATE "7/18 2021 12:16pm"
+#define BUILD_DATE "6/9 2026 2:16am"
 
 #endif
